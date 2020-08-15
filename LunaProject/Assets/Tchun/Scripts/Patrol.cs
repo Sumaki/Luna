c68eb0892fb32aaa20cd68f7cddc2c15c0c26202
Tchun Chao <tchun@live.fr>
2020-08-15 00:19:55 -0400

    AI Patrol script done
    You can use objects to set waypoints between which the AI which follow in order

---
+++ LunaProject/Assets/Tchun/Scripts/Patrol.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int speed;
+
+    private int waypointIndex;
+    private float distance;
+
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if(distance < 1f)
+        {
+            IncreaseIndex();
+        }
+        PatrolPath();
+    }
+
+    void PatrolPath()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+}
