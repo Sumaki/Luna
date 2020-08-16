5a360658e74046dc5e7060e7de6469b7e3ad9a31
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2020-08-16 12:30:52 -0400

    Updated Prefabs in Phi Dai/Prefabs
    UI on click function, disable mouse after closing UI script added.

--- LunaProject/Assets/Phi Dai/Scripts/Gameplay/GameManager.cs
+++ LunaProject/Assets/Phi Dai/Scripts/Gameplay/GameManager.cs
@@ -5,25 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    public GameObject ui;
-    bool ui_Status = true; 
-
-    
-    void Start()
-    {
-        // Turn off mouse at start
-        Cursor.visible = false;
+  
+    public void MouseTurnOff()
+    {      
+            Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        ChangeUIStatus();
-    }
-
-    void ChangeUIStatus()
-    {
-        if (!ui_Status)
-            ui.SetActive(false);
-    }
 }
