bd9d3344ee6f3324d18293c02db832409bc01917
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2020-08-16 15:02:45 -0400

    Teleport SFX + Script Updated

--- LunaProject/Assets/Phi Dai/Scripts/Gameplay/TeleportPlayer.cs
+++ LunaProject/Assets/Phi Dai/Scripts/Gameplay/TeleportPlayer.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class TeleportPlayer : MonoBehaviour
 {
     public Transform destination;
+    public AudioSource teleportSFX;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            other.transform.gameObject.GetComponent<CharacterController>().enabled = false;
-            other.transform.gameObject.transform.position = destination.position;
-            other.transform.gameObject.GetComponent<CharacterController>().enabled = true;
-
+                other.transform.gameObject.GetComponent<CharacterController>().enabled = false;
+                other.transform.gameObject.transform.position = destination.position;
+                other.transform.gameObject.GetComponent<CharacterController>().enabled = true;
+            // play sfx for teleport    
+            if (!teleportSFX.isPlaying)
+                teleportSFX.Play();
         }
     }
 
