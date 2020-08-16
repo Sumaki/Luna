279a994491cad1ff3358e501b93bfbc53a1d24d0
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2020-08-16 17:28:19 -0400

    Added EndGame Script + Updated starting UI

---
+++ LunaProject/Assets/Phi Dai/Scripts/Gameplay/EndGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+
+    public GameObject endingCanvas;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            endingCanvas.GetComponent<Animator>().SetBool("fade", true);
+        }
+    }
+}
